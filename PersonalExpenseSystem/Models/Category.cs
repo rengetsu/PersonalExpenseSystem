24957df48707af82b6390525f46f1bd4ed3899da
Pavel Trostianko <rengetsu@mail.ru>
2023-11-27 17:52:17 +0200

    Category and Trasnsaction models added

---
+++ PersonalExpenseSystem/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalExpenseSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string CategoryIcon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string CategoryType { get; set; } = "Expense";
+    }
+}
---
+++ PersonalExpenseSystem/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalExpenseSystem.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId {  get; set; }
+        public Category Category { get; set; }
+
+        public int TransactionAmount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? TransactionNote { get; set; }
+        
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
+    }
+}
